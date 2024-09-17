8aa7de4b3708b0bdcd1fd15646dfb83a7b954fbd
Mansoor-Muhammad-ICD2O1-01 <mansoor.muhammad@student.tdsb.on.ca>
2024-09-17 14:15:55 +0000

    Mansoor Muhammad Lesson 1.3

--- PP1_3.py
+++ PP1_3.py
@@ -1,13 +1,39 @@
-
+'''
+    Lesson 3 - Using Strings
+    Author: Mansoor Muhammad 
+    Date Created: September 17th, 2024 
+    Date Last Modified: September 17th, 2024 
+''' 
 
 def q1():
-  #Write Assignment code here
-  #Remember your code must be indented once
+   #1
+   print("******"," end = ")
+print("******"," end = ") 
+print("******"," end = ") 
+print("******")   
+
+#2
+print("*****"," end = " )
+print("*   *"," end = ")
+print("*   *"," end = ")
+print("*   *"," end = ")
+print("*****")
+
+#3
+print("******"," end = ")
+print(" *  *"," end =  ")
+print("  * *"," end = ")
+print("   * ")
+   #Remember your code must be indented once
 
 def q2():
+   print("Hello ", " end = ")
+   print("World")
   #Write Assignment code here
   
 def q3():
+  print("Man\nBoard\nMan Overboard""")
+      
   #Write Assignment code here
   
 
