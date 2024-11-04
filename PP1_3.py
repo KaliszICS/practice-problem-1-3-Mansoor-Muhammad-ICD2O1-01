9b2ea0dfe3efc267898ca7526df5d59a6a599ac6
Mansoor-Muhammad-ICD2O1-01 <mansoor.muhammad@student.tdsb.on.ca>
2024-11-04 09:42:13 -0500

    Update PP1_3.py

--- PP1_3.py
+++ PP1_3.py
@@ -9,25 +9,25 @@ def q1():
    #1
     print(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n")
 
-#2
-print("* * * * *")
-print("*       *")
-print("*       *")
-print("*       *")
-print("* * * * *")
-
-#3
-print("* * * * * *")
-print(" *       *")
-print("  *     *")
-print("   *   *")
-print("    * *")
-print("     *")
+    #2
+    print("* * * * *")
+    print("*       *")
+    print("*       *")
+    print("*       *")
+    print("* * * * *")
+    
+    #3
+    print("* * * * * *")
+    print(" *       *")
+    print("  *     *")
+    print("   *   *")
+    print("    * *")
+    print("     *")
    #Remember your code must be indented once
 
 def q2():
-   print("Hello ", " end = ")
-   print("World")
+   print("Hello" end = " ")
+   print("World\n")
   #Write Assignment code here
   
 def q3():
@@ -36,8 +36,9 @@ def q3():
   #Write Assignment code here
   
 
-
+'''
 #Do not alter anything after this line
 q1()
 q2()
 q3()
+'''
